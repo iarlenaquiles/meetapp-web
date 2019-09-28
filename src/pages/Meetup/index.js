9efeea0fcf997604acc0a61c617417819a759b5c
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-28 18:19:24 -0300

    validando os campos

--- src/pages/Meetup/index.js
+++ src/pages/Meetup/index.js
@@ -3,10 +3,19 @@ import PropTypes from 'prop-types';
 import { Form, Input, Textarea } from '@rocketseat/unform';
 import { MdAddCircleOutline } from 'react-icons/md';
 import { parseISO } from 'date-fns';
+import * as Yup from 'yup';
 import { Container } from './styles';
 import DatePicker from './DatePicker';
 import ImagePicker from './ImagePicker';
 import api from '~/services/api';
+
+const schema = Yup.object().shape({
+  title: Yup.string().required('Título é obrigatório'),
+  description: Yup.string().required('Descrição é obrigatório'),
+  location: Yup.string().required('Localização é obrigatório'),
+  date: Yup.date().required('Data é obrigatório'),
+  file_id: Yup.number().required('Escolha uma imagem'),
+});
 
 export default function Meetup({ match, history }) {
   const [meetup, setMeetup] = useState([]);
@@ -29,7 +38,7 @@ export default function Meetup({ match, history }) {
 
   return (
     <Container>
-      <Form initialData={meetup} onSubmit={handleSubmit}>
+      <Form initialData={meetup} schema={schema} onSubmit={handleSubmit}>
         <ImagePicker name="File" />
         <Input name="title" placeholder="Título do Meetup" />
 
@@ -51,6 +60,7 @@ export default function Meetup({ match, history }) {
     </Container>
   );
 }
+
 Meetup.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
