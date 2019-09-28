fddfa55abefbf0115b2e16dd4345093a9512fe42
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-27 23:03:25 -0300

    criando a page do profile

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { MdAddCircleOutline } from 'react-icons/md';
+import { Form, Input } from '@rocketseat/unform';
+import { Container } from './styles';
+import AvatarInput from './AvatarInput';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+import { signOut } from '~/store/modules/auth/actions';
+
+export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Sua nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">
+          <MdAddCircleOutline size={20} />
+          Salvar perfil
+        </button>
+      </Form>
+    </Container>
+  );
+}
