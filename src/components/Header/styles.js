5a64247b1d051d277452ca1b2e2eb668fb4cf5cc
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-27 22:27:43 -0300

    criando header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  background: #000;
+  padding: 0 30px;
+`;
+
+export const Content = styled.div`
+  height: 64px;
+  max-width: 900px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  nav {
+    display: flex;
+    align-items: center;
+
+    img {
+      margin-right: 20px;
+      padding-right: 20px;
+      border-right: 1px solid #eee;
+    }
+
+    a {
+      font-weight: bold;
+      color: #7159c1;
+    }
+  }
+  aside {
+    display: flex;
+    align-items: center;
+
+    button {
+      margin-left: 5px;
+      background: #e5556e;
+      font-weight: bold;
+      height: 44px;
+      width: 50px;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#e5556e')};
+      }
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  margin-left: 20px;
+  padding-left: 20px;
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #fff;
+    }
+
+    a {
+      display: block;
+      margin-top: 2px;
+      font-size: 12px;
+      color: #999;
+    }
+  }
+`;
