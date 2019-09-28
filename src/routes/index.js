2cfaa75d58a2b348d1412612bfe379b0b2f4fd35
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-28 20:45:48 -0300

    criando route de editar

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ import Signup from '~/pages/Signup';
 import Dashboard from '~/pages/Dashboard';
 import Profile from '~/pages/Profile';
 import Meetup from '~/pages/Meetup';
+import Details from '~/pages/Details';
 
 export default function Routes() {
   return (
@@ -16,6 +17,8 @@ export default function Routes() {
       <Route path="/dashboard" component={Dashboard} isPrivate />
       <Route path="/profile" component={Profile} isPrivate />
       <Route path="/meetup" component={Meetup} isPrivate />
+      <Route path="/meetup/edit/:id" component={Meetup} isPrivate />
+      <Route path="/details/:id" component={Details} isPrivate />
     </Switch>
   );
 }
