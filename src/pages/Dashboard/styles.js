09271525ab59e589d44682240743f434750031bb
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-27 23:15:55 -0300

    criando style da dash

---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
+
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    strong {
+      font-size: 24px;
+      color: #fff;
+      font-weight: bold;
+      font-family: 'Roboto', sans-serif;
+    }
+
+    button {
+      display: flex;
+      flex-direction: row;
+      align-self: flex-end;
+      align-items: center;
+      margin: 5px 0 0;
+      height: 44px;
+      padding: 5px;
+      background: #f94d6a;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#F94D6A')};
+      }
+    }
+
+    svg {
+      align-self: flex-start;
+      margin-right: 5px;
+    }
+    strong {
+      color: #fff;
+      font-size: 24px;
+      margin: 0 15px;
+    }
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 15px;
+    margin-top: 30px;
+  }
+`;
+
+export const Time = styled.li`
+  padding: 20px;
+  border-radius: 4px;
+  background: #fff;
+
+  opacity: ${props => (props.past ? 0.6 : 1)};
+
+  strong {
+    display: block;
+    color: ${props => (props.available ? '#999' : '#7159c1')};
+    font-size: 20px;
+    font-weight: normal;
+  }
+
+  span {
+    display: block;
+    margin-top: 3px;
+    color: ${props => (props.available ? '#999' : '#666')};
+  }
+`;
